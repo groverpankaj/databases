98b7e8207cf4e0d7b0ad4841480e57e3f0414843
groverpankaj <pgrover2079@gmail.com>
2020-01-09 10:28:46 -0800

    Add Tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,33 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR (40) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR (40) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR (150) NOT NULL,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  created_at DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (room_id) REFERENCES rooms (id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
+      mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
